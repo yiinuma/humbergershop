3beb258344ef6cab80cf3de27022f10aa30780dc
yiinuma <y.iinuma211@gmail.com>
2020-11-20 23:04:57 +0900

    first1120

---
+++ assets/js/slide.js
@@ -0,0 +1,23 @@
+$(function () {
+  // 「#sidebar-button--open」のclickイベント→「#sidebar」を開く
+  $("#sidebar-button--open").on("click", function () {
+    $("#sidebar").addClass("is-open");
+  });
+
+  // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
+  $("#sidebar-button--close").on("click", function () {
+    $("#sidebar").removeClass("is-open");
+  });
+
+  // 画面サイズがPC以上になったらサイドバーを開いた状態でもPC用のデフォルト画面に戻す;
+  var $win = $(window);
+
+  $win.on("load resize", function () {
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth >= 1280) {
+      // PCの処理
+      $("#sidebar").removeClass("is-open");
+    }
+  });
+});
