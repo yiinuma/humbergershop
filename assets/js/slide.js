774d70daa42dd7683ac87c49750a5463b381ef1e
yiinuma <y.iinuma211@gmail.com>
2020-11-29 08:40:36 +0900

    menu表示に背景固定

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -3,12 +3,14 @@ $(function () {
   $("#sidebar-button--open").on("click", function () {
     $("#sidebar").addClass("is-open");
     $(".p-sidebar__back").addClass("is-open");
+    $("body").addClass("is-open");
   });
 
   // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
   $("#sidebar-button--close").on("click", function () {
     $("#sidebar").removeClass("is-open");
     $(".p-sidebar__back").removeClass("is-open");
+    $("body").removeClass("is-open");
   });
 
   // 画面サイズがPC以上になったらサイドバーを開いた状態でもPC用のデフォルト画面に戻す;
@@ -17,10 +19,11 @@ $(function () {
   $win.on("load resize", function () {
     var windowWidth = window.innerWidth;
 
-    if (windowWidth >= 1280) {
+    if (windowWidth >= 961) {
       // PCの処理
       $("#sidebar").removeClass("is-open");
       $(".p-sidebar__back").removeClass("is-open");
+      $("body").removeClass("is-open");
     }
   });
 });
